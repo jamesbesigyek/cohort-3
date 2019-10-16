b74fb967a8a73a060bcb7038fb006cb6a0600eb5
besigye <jamesbesigyek@gmail.com>
2019-10-16 13:22:50 +0300

    completed the Marks function

--- Judicial/Jamesbesigye/marksfunction.js
+++ Judicial/Jamesbesigye/marksfunction.js
@@ -2,7 +2,7 @@
 
 function marks(mark1,mark2){
   var mark1, mark2, highermark
-highermark = (mark1 >= mark2)?mark1:mark2
+highermark = (mark1 >= mark2)?mark1:mark2 //arrow function compares two test marks and returns the higher marks
 
 
 return highermark
@@ -11,13 +11,24 @@ return highermark
 
 //function to recieve course work marks, add the higher score from marks function and multiply by 40%
 function coursework(mark3){
-  var mark3, finalassesment 
-  var assesment = marks(68,75)
+  var mark3, finalAssesment 
+  var testmark = marks(68,75)
   //console.log(mark3)
-  finalassesment = (mark3 + assesment)*0.4
-  //console.log(assesment)
-  console.log(finalassesment)
-  return finalassesment
+  finalAssesment = (mark3 + testmark)*0.4   // adds test marks to best test mark and multiplies result by 40%
+  //console.log(testmark)
+  //console.log(finalassesment)
+  return finalAssesment                     //returns final assement mark for use by calling function
 
 }
 coursework(25)
+
+function exam(mark4){
+  var finalResults
+  var examMark = mark4*0.6
+  var assesmentMark = coursework(25)
+  finalResults = examMark + assesmentMark
+  console.log(assesmentMark)                //prints final assesment mark
+  console.log(examMark)                     // prints exam mark
+  console.log(finalResults)                 //prints final exam marks
+}
+exam(50)
