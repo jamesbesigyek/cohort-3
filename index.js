54acbd8589a332a854c6d4264ee050ed099e1033
Ahmed Shayo <ahmedshayo014@gmail.com>
2019-11-06 09:15:51 +0300

    This is my second node activity

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.listen(3000, ()=>{
+    console.log('listening on 3000')
+})
+
+app.get('/', (req,res)=>{
+    res.send('Hello World')
+})
+
+app.get('/About', (req,res)=>{
+    res.send('About Us')
+})
